10c437e5eb22ab248bee6d83fb6e464e30d9aec8
Sally Akrawi <sallyakrawi2@yahoo.com>
2023-02-06 14:35:45 -0500

    add the classes

--- MusicBoxProj/MusicBoxProj/Data/Migrations/20230206190641_Initial.Designer.cs
+++ MusicBoxProj/MusicBoxProj/Data/Migrations/20230206190641_Initial.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MusicBoxProj.Data;
 
@@ -11,9 +12,10 @@ using MusicBoxProj.Data;
 namespace MusicBoxProj.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230206190641_Initial")]
+    partial class Initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ MusicBoxProj/MusicBoxProj/Models/DataModels.cs
@@ -0,0 +1,70 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MusicBoxProj.Models
+{
+    public class Album
+    {
+        public int AlbumId { get; set; }
+        public string AlbumName { get; set; } = string.Empty;
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+
+        public int BandId { get; set; }
+        public Band? Band { get; set; }
+        public List<Song>? ListOfSongs { get; set; }
+
+        public List<AlbumGenre>? ListOfGenres { get; set; }
+    }
+
+
+    public class Band
+    {
+        public int BandId { get; set; }
+        [Required]
+        public string BandName { get; set; } = string.Empty;
+        public List<Album>? ListOfAlbums { get; set; }
+        public List<Song>? ListOfSongs { get; set; }
+
+    }
+
+    public class Song
+    {
+        public int SongId { get; set; }
+        [Required]
+        public string SongName { get; set; } = string.Empty;
+
+        public int AlbumId { get; set; }
+        public int BandId { get; set; }
+
+        public int PlayListId { get; set; }
+    }
+
+    public class Genre
+    {
+        public int GenreId { get; set; }
+        [Required]
+        public string GenreName { get; set; } = string.Empty;
+
+        public List<AlbumGenre>? ListOfAlbums { get; set; }
+    }
+
+    public class AlbumGenre
+    {
+        public int AlbumId { get; set; }
+
+        public Album? Album { get; set; }
+
+        public int GenreId { get; set; }
+
+        public Genre? Genre { get; set; }
+
+    }
+
+    public class PlayList
+    {
+        public int PlayListId { get; set; }
+        [Required]
+        public string PlayListName { get; set; } = string.Empty;
+        public List<Song>? ListOfSongs { get; set; }
+    }
+}
